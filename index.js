47b72e13534d42f1d6a35589f8d139d7478a88cd
Jonathan East <jonathan_east@ymail.com>
2020-11-18 14:47:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 function fetchBooks() {
-  fetch('')
+  fetch('https://anapioficeandfire.com/api/books')
+  .then(resp => resp.json())
+  .then(json => renderBooks())
 }
 
 function renderBooks(books) {
